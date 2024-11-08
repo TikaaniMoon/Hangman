98dac96a84a5fbec10c04749abd816b5ae1838b1
Tikaani <dara.stormy@gmail.com>
2024-11-08 12:04:35 +0200

    feat: add conditions for guessing the word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-clear = lambda: os.system
+
+clear = lambda: os.system('cls')
 
 print('Привет! Я загадал слово, твоя задача - угадать его по буквам.')
 input('*Нажми Enter, чтобы продолжить*')
@@ -8,5 +9,16 @@ print('Поехали!')
 
 word = 'трагикомедия'
 
-for symb in word:
-    print(symb, end=' ')
+letters = []
+
+while True:
+    letter = input('Введите букву: ')
+    letters.append(letter)
+    print(letters)
+    for symb in word:
+        if symb in letters:
+            print(symb, end=' ')
+        else:
+            print('*', end=' ')
+    print()
+
